a8302a564a2b4b3a022e501a2606189908a179aa
Roman Klimenko <romaklimenko@gmail.com>
2015-11-18 18:42:05 +0100

    gulp less, .gitignore, typings

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('less', function () {
+  return gulp.src('./less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./dist/css'));
 });
