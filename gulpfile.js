4f23fd0bbba51656113c7e7b98c12f2feb2025ef
Roman Klimenko <romaklimenko@gmail.com>
2015-11-28 21:46:44 +0100

    Get rid of babel ans source maps. Watch task added.

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,9 @@
 var path        = require('path');
 
 // node_modules
-var babel       = require('gulp-babel');
 var del         = require('del');
 var gulp        = require('gulp');
 var less        = require('gulp-less');
-var sourcemaps  = require('gulp-sourcemaps');
 var ts          = require('gulp-typescript');
 
 var tsProject = ts.createProject('tsconfig.json');
@@ -14,21 +12,25 @@ var tsProject = ts.createProject('tsconfig.json');
 gulp.task('default', ['copy', 'less', 'ts']);
 
 gulp.task('clean', function(){
-  return del(['dist/**/*']);
+  return del(['dist/**/*', '!dist/css/**', '!dist/js/**', '!dist/main.js']);
 });
 
 gulp.task('copy', ['clean'], function () {
-  gulp.src([
+  return gulp.src([
       'index.html',
       'package.json',
-      '*css/**/*',
       '*img/**/*',
-      '*lib/**/*'
+      '*lib/**/*',
+      '*typings/**/*'
   ])
   .pipe(gulp.dest('dist'));
 });
 
-gulp.task('less', ['clean'], function () {
+gulp.task('css-clean', function() {
+  return del(['dist/css/**/*']);
+});
+
+gulp.task('less', ['css-clean'], function () {
   return gulp.src('less/**/*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less') ]
@@ -36,11 +38,17 @@ gulp.task('less', ['clean'], function () {
     .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('ts', ['clean'], function(){
+gulp.task('ts-clean', function() {
+  return del(['dist/js/**/*', 'dist/main.js']);
+});
+
+gulp.task('ts', ['ts-clean'], function(){
   return gulp.src(['ts/**/*.ts', 'main.ts'])
-    .pipe(sourcemaps.init())
     .pipe(ts(tsProject))
-    .pipe(babel())
-    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['ts/**/*.ts', 'main.ts'], ['ts']);
+  gulp.watch(['less/**/*.less'], ['less']);
 });
