f892de44faf7f80f097c8f2f3c8d4e5e0721c3cb
Roman Klimenko <romaklimenko@gmail.com>
2015-12-25 16:19:15 +0100

    Initial redux

--- scripts/Core.tsx
+++ scripts/Core.tsx
@@ -1,6 +1,28 @@
 'use strict';
 
+import { createStore } from 'redux';
 import { Container } from './Components/Container';
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+  case 'INCREMENT':
+    return state + 1;
+  case 'DECREMENT':
+    return state - 1;
+  default:
+    return state;
+  }
+};
+
+const store = createStore(counter);
+
+store.subscribe(() =>
+  console.log(store.getState())
+);
+
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'DECREMENT' });
 
 const containerNode: any = document.getElementById('container');
 ReactDOM.render(<Container />, containerNode);
