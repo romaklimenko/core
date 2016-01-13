9e8dae76eeb3bd5e95c91dc2df969b0e9b93c59e
Roman Klimenko <romaklimenko@gmail.com>
2016-01-13 18:49:38 +0100

    #10 the tree is flat now

--- scripts/components/content.js
+++ scripts/components/content.js
@@ -4,15 +4,15 @@ const React = require('react')
 
 const Content = (props) => {
   const currentTreeNode = props.state.get('currentTreeNode')
-  const currentTreeNodeJson = currentTreeNode ?
-    JSON.stringify(currentTreeNode.toJS(), null, '\t') : ''
 
-  return React.createElement(
-    'div',
-    { className: 'content' },
-    React.createElement(
-      'pre',
-      { style: { overflow: 'hidden', margin: '25px', maxWidth: '800px' } },
-      currentTreeNodeJson )) }
+  return React.createElement('div', { className: 'content' },
+    React.createElement('p', {
+      style: {
+        overflow: 'hidden',
+        margin: '25px',
+        maxWidth: '800px' }
+      },
+      currentTreeNode ))
+}
 
 module.exports = Content
--- scripts/components/tree/tests/tree-reducers-test.js
+++ scripts/components/tree/tests/tree-reducers-test.js
@@ -2,22 +2,82 @@
 
 const test = require('tape')
 
-const Immutable = require('immutable')  
+const Immutable = require('immutable')
 
 const InitialState = require('../../../initial-state')
-const TreeReducer = require('../tree-reducers')
+const TreeConstants = require('../tree-constants')
+const TreeReducers = require('../tree-reducers')
 
-test('TreeReducer does not process @@redux/INIT.', (assert) => {
+test('@@redux/INIT', (assert) => {
   const state = Immutable.fromJS({a: 'a', b: { c: 'c' }})
   const action = { type: '@@redux/INIT' }
-  const newState = TreeReducer(state, action)
+  const newState = TreeReducers.TreeReducer(state, action)
   assert.equal(state, newState)
   assert.end()
 })
 
 test('TreeReducer returns InitialState if passed state is undefined.', (assert) => {
   const action = { type: '@@redux/INIT' }
-  const state = TreeReducer(undefined, action)
+  const state = TreeReducers.TreeReducer(undefined, action)
   assert.equal(state, InitialState)
+  assert.end()
+})
+
+test('TreeReducer.TREE_COLLAPSE', (assert) => {
+  const action = { type: TreeConstants.TREE_COLLAPSE, path: '11111111-1111-1111-1111-111111111111/A' }
+
+  const state = InitialState
+    .setIn(
+      ['tree', '11111111-1111-1111-1111-111111111111/A'],
+      Immutable.fromJS({ path: '11111111-1111-1111-1111-111111111111/A' }))
+    .setIn(
+      ['tree', '11111111-1111-1111-1111-111111111111/A/AA'],
+      Immutable.fromJS({ path: '11111111-1111-1111-1111-111111111111/A/AA' }))
+    .setIn(
+      ['tree', '11111111-1111-1111-1111-111111111111/A/AA/AAA'],
+      Immutable.fromJS({ path: '11111111-1111-1111-1111-111111111111/A/AA/AAA' }))
+    .setIn(
+      ['tree', '11111111-1111-1111-1111-111111111111/A/AB'],
+      Immutable.fromJS({ path: '11111111-1111-1111-1111-111111111111/A/AB' }))
+
+  assert.equal(state.get('tree').count(), 5)
+
+  const newState = TreeReducers.TreeReducer(state, action)
+
+  assert.equal(newState.get('tree').count(), 2)
+
+  assert.end()
+})
+
+test('TreeReducer.TREE_EXPAND', (assert) => {
+  const action = { type: TreeConstants.TREE_EXPAND, path: '11111111-1111-1111-1111-111111111111' }
+
+  const state = InitialState
+
+  const newState = TreeReducers.TreeReducer(state, action)
+
+  assert.equal(newState.getIn(['tree', action.path, 'loading']), true)
+  assert.end()
+})
+
+test('TreeReducer.TREE_FETCH_RESPONSE', (assert) => {
+  const action = {
+    type: TreeConstants.TREE_FETCH_RESPONSE,
+    path: '11111111-1111-1111-1111-111111111111',
+    children: [
+      { Path: '11111111-1111-1111-1111-111111111111/A/AA' },
+      { Path: '11111111-1111-1111-1111-111111111111/A/AB' },
+      { Path: '11111111-1111-1111-1111-111111111111/A/AC' }
+    ]
+  }
+
+  const state = InitialState.setIn(['tree', '11111111-1111-1111-1111-111111111111', 'loading'], true)
+  assert.equal(state.get('tree').count(), 1)
+  assert.equal(state.getIn(['tree', action.path, 'loading']), true)
+
+  const newState = TreeReducers.TreeReducer(state, action)
+
+  assert.equal(newState.getIn(['tree', action.path, 'loading']), false)
+  assert.equal(newState.get('tree').count(), 4)
   assert.end()
 })
