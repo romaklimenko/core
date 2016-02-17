e8a1b705079d155cc2502297df87bb703f837c75
Roman Klimenko <romaklimenko@gmail.com>
2016-02-17 20:38:39 +0100

    Do not run Sitecore repository tests on server

--- scripts/repositories/tests/sitecore-repository-test.ts
+++ scripts/repositories/tests/sitecore-repository-test.ts
@@ -1,10 +1,17 @@
-import test from 'tape'
+import * as tape from 'tape'
 
 import SitecoreRepository from '../sitecore-repository'
 
 const templatesItemId = '{3C1715FE-6A13-4FCF-845F-DE308BA9741D}'
 
-// TODO: do not run on TravisCI
+let test: any = tape.default
+
+if (process.env.TRAVIS) {
+    test = (name) => {
+      console.warn('"' + name
+        + '" is disabled as there is no Sitecore instance available on Travis CI.')
+    }
+}
 
 test('SitecoreRepository.getChildren', (assert) => {
   SitecoreRepository.getChildren(templatesItemId)
@@ -19,7 +26,7 @@ test('SitecoreRepository.getChildren', (assert) => {
 test('SitecoreRepository.getItem', (assert) => {
   SitecoreRepository.getItem(templatesItemId)
     .then(item => {
-      assert.equal(item['ID'], templatesItemId) // TODO: need IItem
+      assert.equal(item['ID'], templatesItemId)
       assert.end() })
     .catch(reason => {
       assert.fail(reason)
